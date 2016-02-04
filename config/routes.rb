1ba54393de9bea357c86023d7b7f68a17e0550ce
A-Man-in-Tux <nchristian77@gamil.com>
2016-02-04 22:36:03 +0000

    add log-in system and rough ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  get 'signup' => 'users#new'
+  
+  root 'static_pages#home'
 
-  get 'users/show'
+  get    'about'  => 'static_pages#about'
+
+  get    'login'  => 'sessions#new'
+  post   'login'  => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+  
+  get    'signup' => 'users#new'
+
   
   resources :users
 
